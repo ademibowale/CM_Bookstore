439b8b950a5583c48c9a74bd40c976c0c31d5730
adebowale <ademibowaleadegboye@gmail.com>
2022-08-11 18:13:23 +0100

    setup react

---
+++ my-app/src/Components/Books/Books.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+import Book from './Book';
+import AddBook from './AllBook';
+import Header from '../Navigation/Header';
+// import { getBooks } from '../../redux/books/books';
+import './Books.css';
+
+export default function Books() {
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, []);
+  return (
+    <>
+      <Header />
+      <div className="books_container">
+        {books.map((book) => (
+          <Book key={book.item_id} book={book} />
+        ))}
+      </div>
+      <AddBook />
+    </>
+  );
+}
